5aad2c0df03d96096d9e9b4b824801a1ed937ffa
Kirill Greshnov <k.greshnov@innopolis.university>
2025-07-06 21:30:59 +0300

    feat: add handlers and setup routes for activity, nutrition, and social services

--- backend/nutrition-service/internal/handler/nutrition.go
+++ backend/nutrition-service/internal/handler/nutrition.go
@@ -1,1 +1,10 @@
 package handler
+
+import (
+	"net/http"
+)
+
+func NutritionHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Nutrition handler"))
+}
